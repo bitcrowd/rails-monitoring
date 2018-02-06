0d1230f79b2b1740708ecb1a7d5374568faec70d
Julian Dobmann <julian@bitcrowd.net>
2018-02-06 17:52:40 +0100

    [#6] Time zone differences

--- app/services/sidekiq_monitoring/status.rb
+++ app/services/sidekiq_monitoring/status.rb
@@ -2,7 +2,7 @@ module SidekiqMonitoring
   class Status
     def self.refresh
       Sidekiq.redis do |conn|
-        conn.set('monitoring:timestamp:whenever_ran', Time.current.strftime('%Y-%m-%d %H:%M:%S'))
+        conn.set('monitoring:timestamp:whenever_ran', Time.current.to_s(:db))
       end
       RefreshStatusJob.perform_later
     end
